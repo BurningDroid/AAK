3f2042ca2d81ef607cd9805755b932cf2c1633ed
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2022-09-03 01:49:39 +0900

    generic 개선

--- aak/src/main/java/dev/aaron/aak/common/ResultExt.kt
+++ aak/src/main/java/dev/aaron/aak/common/ResultExt.kt
@@ -1,4 +1,4 @@
 package dev.aaron.aak.common
 
-fun <T> failureOf(failedResult: Result<T>): Result<T> =
+fun <T> failureOf(failedResult: Result<Any>): Result<T> =
     Result.failure(failedResult.exceptionOrNull() ?: Exception())
