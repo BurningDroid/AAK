98a35dffb1fee60544127db080fd5e313bf8c46c
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2022-09-23 23:57:52 +0900

    CurrencyTransformation

---
+++ aak/src/main/java/dev/aaron/aak/compose/text/input/CurrencyTransformation.kt
@@ -0,0 +1,32 @@
+package dev.aaron.aak.compose.text.input
+
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+import dev.aaron.aak.number.toSafeDouble
+import java.text.DecimalFormat
+
+private val decimalFormat: DecimalFormat by lazy { DecimalFormat() }
+
+class CurrencyTransformation : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        return TransformedText(
+            text = AnnotatedString(text.text.toCurrencyFormat()),
+            offsetMapping = object : OffsetMapping {
+                override fun originalToTransformed(offset: Int): Int {
+                    return text.text.toCurrencyFormat().length
+                }
+
+                override fun transformedToOriginal(offset: Int): Int {
+                    return text.length
+                }
+            }
+        )
+    }
+}
+
+private fun String.toCurrencyFormat(): String {
+    val newText = decimalFormat.format(this.toSafeDouble())
+    return if (this.endsWith(".")) "$newText." else newText
+}
