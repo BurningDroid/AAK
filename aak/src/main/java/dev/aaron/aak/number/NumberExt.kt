ff2956195231caf4cc8eacc24b48c169c5d17f19
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2022-09-26 23:05:09 +0900

    round

--- aak/src/main/java/dev/aaron/aak/number/NumberExt.kt
+++ aak/src/main/java/dev/aaron/aak/number/NumberExt.kt
@@ -1,7 +1,20 @@
 package dev.aaron.aak.number
 
+import kotlin.math.pow
+import kotlin.math.round
+
 fun String?.toSafeInt(default: Int = 0) = this?.trim()?.toIntOrNull() ?: default
 
 fun String?.toSafeFloat(default: Float = 0F) = this?.trim()?.toFloatOrNull() ?: default
 
 fun String?.toSafeDouble(default: Double = 0.0) = this?.trim()?.toDoubleOrNull() ?: default
+
+fun Double.round(digit: Int = 2): Double {
+    val v = 10F.pow(digit)
+    return round(this * v) / v
+}
+
+fun Float.round(digit: Int = 2): Float {
+    val v = 10F.pow(digit)
+    return round(this * v) / v
+}
