8e491d82e6f77636c483eb198a85c24b7786e54d
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2022-11-03 01:42:37 +0900

    sample app

---
+++ app/src/main/java/dev/aaron/MainActivity.kt
@@ -0,0 +1,27 @@
+package dev.aaron
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import dev.aaron.ui.theme.SampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SampleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Text(text = "Hello World")
+                }
+            }
+        }
+    }
+}
